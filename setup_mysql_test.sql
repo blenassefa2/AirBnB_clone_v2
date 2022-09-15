e2c0f29760b61206c52769bae26aa5be3ea1f638
blenassefa2 <blenassefa59@yahoo.com>
2022-09-15 18:45:05 +0300

    tried fixing task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,17 +3,18 @@
 -- Create database named hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Create user of the database
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- use the database
+USE `hbnb_test_db`;
 
--- set password to user
-SET PASSWORD FOR 'hbnb_test'@'localhost' = PASSWORD('hbnb_test_pwd');
+-- Create user of the database
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- give all the user's properties to the database
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost'
+	IDENTIFIED BY 'hbnb_test_pwd';
 
 -- let the user see the perfomance data
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
 -- avoid any errors by other user privileges
-FLUSH PRIVILEGES;
+-- FLUSH PRIVILEGES;
