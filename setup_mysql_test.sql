d7f08db1ccf8adb31fa8dac09f59fd6fda682b91
blenassefa2 <blenassefa59@yahoo.com>
2022-09-15 18:36:12 +0300

    tried task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -12,7 +12,8 @@ SET PASSWORD FOR 'hbnb_test'@'localhost' = PASSWORD('hbnb_test_pwd');
 -- give all the user's properties to the database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
---let the user see the perfomance data
+-- let the user see the perfomance data
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
+-- avoid any errors by other user privileges
 FLUSH PRIVILEGES;
