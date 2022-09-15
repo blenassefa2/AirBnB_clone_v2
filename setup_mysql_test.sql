fdf4eaaf7ccbd63a3112870b6c61556dfb4e9828
blenassefa2 <blenassefa59@yahoo.com>
2022-09-15 18:54:30 +0300

    tried task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,14 +7,14 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 USE `hbnb_test_db`;
 
 -- Create user of the database
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- give all the user's properties to the database
-GRANT ALL PRIVILEGES ON 'hbnb_test_db' TO 'hbnb_test'@'localhost'
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
 	IDENTIFIED BY 'hbnb_test_pwd';
 
 -- let the user see the perfomance data
-GRANT SELECT ON 'performance_schema' TO 'hbnb_test'@'localhost'
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
 	IDENTIFIED BY 'hbnb_test_pwd';
 
 -- avoid any errors by other user privileges
